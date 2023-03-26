1a45cc30a9f10691ef3e448fe899b643b70f4de9
lsunyoung <zhd8322@naver.com>
2023-03-26 13:22:03 +0900

    버튼 눌렀을 때 toast 팝업 구현

--- app/src/main/java/com/example/test2/MainActivity.kt
+++ app/src/main/java/com/example/test2/MainActivity.kt
@@ -2,12 +2,24 @@ package com.example.test2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    val aaa = 1
+    val bbb = 2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val aaa = 1
-        val bbb = 2
+        onButton1Clicked()
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            onButton1Clicked()
+        }
     }
+
+    public fun onButton1Clicked() {
+        Toast.makeText(this, "버튼이 눌렸어요.",Toast.LENGTH_LONG).show()
+    }
+
 }
