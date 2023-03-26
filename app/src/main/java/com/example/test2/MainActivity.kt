a14b018628c02fe9244a87b70fec2d398c175532
lsunyoung <zhd8322@naver.com>
2023-03-26 12:40:22 +0900

    test 입니다!

--- app/src/main/java/com/example/test2/MainActivity.kt
+++ app/src/main/java/com/example/test2/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val aaa = 1
+        val bbb = 2
     }
 }
