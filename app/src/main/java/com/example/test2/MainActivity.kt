9b08f3e5eee97cb12f77eb1e4a359608046867a8
lsunyoung <zhd8322@naver.com>
2023-03-26 15:07:49 +0900

    버튼명 토스트팝업명 변경

--- app/src/main/java/com/example/test2/MainActivity.kt
+++ app/src/main/java/com/example/test2/MainActivity.kt
@@ -8,18 +8,21 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     val aaa = 1
     val bbb = 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        onButton1Clicked()
 
+        Button()
+    }
+    fun Button() {
         findViewById<Button>(R.id.button).setOnClickListener {
-            onButton1Clicked()
+            showToast()
         }
     }
-
-    public fun onButton1Clicked() {
-        Toast.makeText(this, "버튼이 눌렸어요.",Toast.LENGTH_LONG).show()
+    fun showToast() {
+        val toast = Toast.makeText(this, "토스트는 이삭이지!",Toast.LENGTH_SHORT)
+        toast.show()
     }
 
 }
